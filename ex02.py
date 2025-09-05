25a7a0dc939f5f00a58f9b19ea4e7eb68627988c
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-04 21:44:34 -0300

    Exercício 2 finalizado

---
+++ ex02.py
@@ -0,0 +1,15 @@
+'''
+    Autores: Glauber Almeida B. - Luis Henrique Nunes C. P.
+    Turma: 2ºA DS               Data: 04/09/2025
+
+    Exercício 02
+    - Crie um programa em que o usuário digite um número e fale se ele é par ou impar.
+
+'''
+
+num = int(input("Digite um número: "))
+
+if num % 2 == 1:
+    print("O número é impar.")
+else:
+    print("Número é par.")
