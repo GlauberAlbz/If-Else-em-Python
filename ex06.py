593cced6ce1e68e777b3fa3f172cae0c37694858
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-04 22:10:15 -0300

    Exercício 06 finalizado

--- ex06.py
+++ ex06.py
@@ -6,3 +6,18 @@
     - Faça um programa que pegue a medida de 3 retas e fale se essas retas formam um triângulo.
 
 '''
+
+reta1 = float(input("Digite o valor da reta 1: "))
+reta2 = float(input("Digite o valor da reta 2: "))
+reta3 = float(input("Digite o valor da reta 3: "))
+
+if reta1 + reta2 > reta3:
+    if reta2 + reta3 > reta1:
+        if reta3 + reta2 > reta1:
+            print("É possível montar um triângulo")
+        else:
+            print("Não é possível montar um triângulo")
+    else:
+        print("Não é possível montar um triângulo")
+else:
+    print("Não é possível montar um triângulo")
