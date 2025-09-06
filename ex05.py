46ee893b874f77cbab532826ca523ea1a852fb34
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-06 19:49:09 -0300

    Exercício 05 finalizado.

--- ex05.py
+++ ex05.py
@@ -8,3 +8,33 @@
     - Se ele tiver filhos, pagar R$200,00 por filho
 
 '''
+
+salario = float(input("Digite o seu sálario: "))
+filhos = int(input("Digite quantos filhos você tem: "))
+bonus_pobre = float()
+bonus_filho = int()
+
+if salario < 2000:
+    reajuste_salario = float()
+    bonus_pobre = salario * 0.15
+    bonus_filho = filhos * 200
+    reajuste_salario = salario + bonus_pobre + bonus_filho
+else:
+    bonus_filho = filhos * 200
+    reajuste_salario = salario + bonus_filho
+
+print("\n")
+print(50*"-")
+if bonus_pobre != 0:
+    print(f"Você recebeu um bônus salárial de: R${bonus_pobre:.2f}. Pois seu salário é menor que R$2000,00.")
+
+if bonus_filho != 0:
+    print(f"Você recebeu um bônus salárial de: R${bonus_filho:.2f}. Pois você possui {filhos} filho(s). ")
+
+if reajuste_salario == salario:
+    print("Você não recebeu nenhum bônus salarial.")
+    print(f"O seu salário é: R${salario:.2f}")
+else:
+    print(f"O seu salário reajustado é de R${reajuste_salario:.2f}")
+
+print(50*"-")
