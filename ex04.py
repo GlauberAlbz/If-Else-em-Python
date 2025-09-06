8e3a00637ec81e1b68319fe13830ae4bf55efe80
GlauberAlbz <glauberalmeidab@gmail.com>
2025-09-06 19:22:19 -0300

    Alteração do exercício 04 - Finalizado

--- ex04.py
+++ ex04.py
@@ -10,6 +10,12 @@
 ano = int(input("Digite um ano para descobrir se é Bissexto: "))
 
 if ano % 4 == 0:
-    print("É bissexto!")
+    if ano % 100 == 0:
+        if ano % 400 == 0:
+            print("É bissexto!")
+        else:
+           print("Não é bissexto!")
+    else:
+        print("É bissexto!")
 else:
     print("Não é bissexto!")
